06261b90227bda1e2d0b809f1ad4c255eeb8f160
tarsyc <czy1474302026@outlook.com>
2023-10-15 21:58:04 +0800

    修复了装甲板编号显示错误

--- src/armors_select.cpp
+++ src/armors_select.cpp
@@ -6,34 +6,35 @@
 using namespace std;
 using namespace cv;
 
-void Armor_manager::armors_select(Mat& frame,vector<Armor>& armors,vector<Armor>& temp_armors,long long int timestamp)
+void Armor_manager::armors_select(Mat& frame, vector<Armor>& armors, vector<Armor>& temp_armors, long long int timestamp)
 {
-    for(int i=0;i<temp_armors.size();i++)
+    const int angleThreshold = 40;
+    const int positionThreshold = 150;
+
+    for (const auto& temp_armor : temp_armors)
     {
-        bool found=false;
-        for(int j=0;j<armors.size();j++)
-        {  
-            if(temp_armors[i].digital=armors[j].digital)
+        bool found = false;
+        for (auto& armor : armors)
+        {
+            if (temp_armor.digital == armor.digital)
             {
-                if(abs(temp_armors[i].angle-armors[j].angle)<40&&abs(temp_armors[i].center.x-armors[j].center.y)<150)//
+                if (abs(temp_armor.angle - armor.angle) < angleThreshold && abs(temp_armor.center.x - armor.center.y) < positionThreshold)
                 {
-                    armors[j].update(temp_armors[i].center,temp_armors[i].width,temp_armors[i].height,temp_armors[i].angle,timestamp);//更新装甲板数据
-                    found=true;
+                    armor.update(temp_armor.center, temp_armor.width, temp_armor.height, temp_armor.angle, timestamp);
+                    found = true;
+                    break;
                 }
             }
         }
-        if(found==false)
-        {
-            Armor armor(frame,temp_armors[i].center,temp_armors[i].width,temp_armors[i].height,temp_armors[i].angle,timestamp);
-            armors.push_back(armor);//创建新的装甲板
 
-        }
-    }
-    for(int i=0;i<armors.size();i++)
-    {
-        if(armors[i].timestamp<timestamp)
+        if (!found)
         {
-            armors.erase(armors.begin()+i);//删除不合格的装甲板
+            Armor armor(frame, temp_armor.center, temp_armor.width, temp_armor.height, temp_armor.angle, timestamp);
+            armors.push_back(armor);
         }
     }
+
+    armors.erase(std::remove_if(armors.begin(), armors.end(), [timestamp](const Armor& armor) {
+        return armor.timestamp < timestamp;
+    }), armors.end());
 }
